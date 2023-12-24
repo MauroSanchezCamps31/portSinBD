05291ec984c831fe1fc67aa564fa7eb00020f648
unknown <maurosanchez@gmail.com>
2023-12-24 16:51:03 -0300

    deployadaptadoarender

--- src/app.js
+++ src/app.js
@@ -9,8 +9,11 @@ const path = require("path");
 app.use(express.static("public"));
 
 // Ponemos a escuchar el servidor
-app.listen(3033, () => {
-    console.log("Servidor corriendo en http://localhost:3033")
+
+const port = process.env.PORT || 3033
+
+app.listen(port, () => {
+    console.log("Servidor corriendo en http://localhost:" + port)
 });
 
 // Definimos las rutas a los distintos pedidos que nuestro sitio sabe responder
